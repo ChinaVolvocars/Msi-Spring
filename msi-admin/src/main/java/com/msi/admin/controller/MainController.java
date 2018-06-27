aa9af54ea6a228c1d3bc7976376e8e2700025d44
Msi <atlantisspeed@gmail.com>
2018-06-27 15:23:45 +0800

    spring项目

--- msi-admin/src/main/java/com/msi/admin/controller/MainController.java
+++ msi-admin/src/main/java/com/msi/admin/controller/MainController.java
@@ -1,12 +1,9 @@
 package com.msi.admin.controller;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/main")
@@ -16,11 +13,11 @@ public class MainController extends BaseController {
 //  @Autowired
 
 
-  @RequestMapping(value = "/login", method = RequestMethod.POST)
+  @RequestMapping(value = "/login", method = RequestMethod.GET)
   @ResponseBody
-  public JSONPObject login(String userName, String userPasswor, HttpServletRequest servletRequest) {
+  public String login() {
     //
-    return null;
+    return "hello world";
   }
 
 }
